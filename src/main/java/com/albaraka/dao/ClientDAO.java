97cd6cfe60723631f82d617996ba046f0a43ff7e
Gabigabe <mhammed.gabigabe@gmail.com>
2026-09-24 16:53:24 +0100

    feat: ajouter la méthode findById du ClientDAO

--- src/main/java/com/albaraka/dao/ClientDAO.java
+++ src/main/java/com/albaraka/dao/ClientDAO.java
@@ -6,6 +6,7 @@ import main.java.com.albaraka.utils.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class ClientDAO {
 
@@ -33,4 +34,46 @@ public class ClientDAO {
             );
         }
     }
+
+    public Optional<Client> findById(Long id) {
+
+        String sql = """
+            SELECT id, nom, email
+            FROM clients
+            WHERE id = ?
+            """;
+
+        try (
+                Connection connection = DatabaseConnection.getConnexion();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+
+            statement.setLong(1, id);
+
+            try (var resultSet = statement.executeQuery()) {
+
+                if (resultSet.next()) {
+
+                    Client client = new Client(
+                            resultSet.getLong("id"),
+                            resultSet.getString("nom"),
+                            resultSet.getString("email")
+                    );
+
+                    return Optional.of(client);
+                }
+
+                return Optional.empty();
+            }
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(
+                    "Erreur lors de la recherche du client",
+                    e
+            );
+        }
+    }
+
+
 }
